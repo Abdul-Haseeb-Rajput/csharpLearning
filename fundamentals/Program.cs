7f631beab3d09f4dfb0c836a0fcb99c7be9165da
Abdul Haseeb Rajput <abdulhaseeb.9820@gmail.com>
2026-09-24 13:34:09 +0500

    first commit

---
+++ fundamentals/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = 12;
+string name = "Abdul Haseeb";
+bool isActive = true;
+decimal taxPercentage = 0.12M;
+DateTime currentDate = DateTime.Now;
+
+Console.WriteLine(number);
+Console.WriteLine(name);
+Console.WriteLine(isActive);
+Console.WriteLine(taxPercentage);
+Console.WriteLine(currentDate);
